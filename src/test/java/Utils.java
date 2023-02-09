9b1e29cef21bc1618f66e8ee05dab5c7188a5316
Sowjanya.Annepu <>
2023-02-09 11:25:44 +0000

    save work

---
+++ src/test/java/Utils.java
@@ -0,0 +1,6 @@
+
+
+public class Utils {
+    public final static String BASE_URL = "https://formy-project.herokuapp.com/form";
+    public final static String CHROME_DRIVER_LOCATION =System.getProperty("user.dir")+ "//vendor/chromedriver";
+}
